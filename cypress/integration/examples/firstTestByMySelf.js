7e76bfc74990611e3383c7014432117c510bac02
Piotr Foltyn <piotrf@apr.pl>
2020-11-19 11:30:22 +0100

    First test wroten pairly by my self

---
+++ cypress/integration/examples/firstTestByMySelf.js
@@ -0,0 +1,12 @@
+describe('First test', () => {
+
+    it('PageTitleValidation - Positive', () => {
+        cy.visit('https://frontend.nopcommerce.com/');
+        cy.title().should('eq', 'nopCommerce demo store');
+    })
+    it('PageTitleValidation - Negative', () => {
+        cy.visit('https://frontend.nopcommerce.com/');
+        cy.title().should('eq', 'nopCommerce store');
+    })
+
+})
